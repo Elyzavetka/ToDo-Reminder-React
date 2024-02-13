e86d70a43b8c2f73f4001ed6c2741c3c6381ece5
Liza Tarasova <tarasova.liza.vic@gmail.com>
2024-02-13 16:25:24 +0000

    changed styles of TodoList component

--- src/components/Todos/TodoList.js
+++ src/components/Todos/TodoList.js
@@ -1,7 +1,12 @@
 import Todo from "./Todo";
+import styles from './TodoList.module.css'
 
 const TodoList = ({ todos }) => {
-    return todos.map((todo, index) => <Todo key={index} todo={todo} />)
+    return (
+        <div className={styles.todoListContainer}>
+            {todos.map((todo, index) => <Todo key={index} todo={todo} />)}
+        </div>
+    )
 }
 
 export default TodoList;
