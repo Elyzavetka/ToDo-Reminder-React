e970021c93243f76fe93870fe910812d0d5c002e
Liza Tarasova <tarasova.liza.vic@gmail.com>
2024-02-13 18:14:35 +0000

    add react-icon for todos

--- src/components/Todos/Todo.js
+++ src/components/Todos/Todo.js
@@ -1,8 +1,10 @@
+import { RiTodoFill } from 'react-icons/ri';
 import styles from './Todo.module.css';
 
 const Todo = ({ todo, index, deleteTodo }) => {
     return (
         <div className={styles.todo} onDoubleClick={() => deleteTodo(index)}>
+            <RiTodoFill />
             <div className={styles.todoText}>{todo}</div>
         </div>
     )
