1f07e2fdc7ddd12db92e707122300b07bbcd2da3
Liza Tarasova <tarasova.liza.vic@gmail.com>
2024-02-16 19:18:09 +0000

    added onClick to deleteIcon

--- src/components/Todos/Todo.js
+++ src/components/Todos/Todo.js
@@ -5,10 +5,10 @@ import styles from './Todo.module.css';
 const Todo = ({ todo, deleteTodo }) => {
     return (
         <div className={styles.todo} onDoubleClick={() => deleteTodo(todo.id)}>
-            <RiTodoFill className={styles.todoIcon}/>
+            <RiTodoFill className={styles.todoIcon} />
             <div className={styles.todoText}>{todo.text}</div>
-            <RiDeleteBin2Line className={styles.deleteIcon}/>
-            <FaCheck className={styles.checkIcon}/>
+            <RiDeleteBin2Line className={styles.deleteIcon} onClick={() => deleteTodo(todo.id)} />
+            <FaCheck className={styles.checkIcon} />
         </div>
     )
 }
