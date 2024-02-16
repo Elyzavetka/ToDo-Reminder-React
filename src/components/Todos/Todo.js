6433ff7401c87e45c0fc93e41ed1f77c3cb5980e
Liza Tarasova <tarasova.liza.vic@gmail.com>
2024-02-16 15:32:11 +0000

    added icons RiDeleteBin2Line and FaCheck to every todo task

--- src/components/Todos/Todo.js
+++ src/components/Todos/Todo.js
@@ -1,4 +1,5 @@
-import { RiTodoFill } from 'react-icons/ri';
+import { RiTodoFill, RiDeleteBin2Line } from 'react-icons/ri';
+import { FaCheck } from 'react-icons/fa';
 import styles from './Todo.module.css';
 
 const Todo = ({ todo, deleteTodo }) => {
@@ -6,6 +7,8 @@ const Todo = ({ todo, deleteTodo }) => {
         <div className={styles.todo} onDoubleClick={() => deleteTodo(todo.id)}>
             <RiTodoFill className={styles.todoIcon}/>
             <div className={styles.todoText}>{todo.text}</div>
+            <RiDeleteBin2Line />
+            <FaCheck />
         </div>
     )
 }
