b0ef5b762f8af5edac9409c35f0ff2fb13ad8392
Liza Tarasova <tarasova.liza.vic@gmail.com>
2024-02-18 13:54:11 +0000

    added styles to TodoActions component

--- src/components/Todos/TodosActions.js
+++ src/components/Todos/TodosActions.js
@@ -1,12 +1,13 @@
 import { RiDeleteBin2Line, RiRefreshLine } from 'react-icons/ri';
 import Button from '../UI/Button'
+import styles from './TodoActions.module.css'
 
 const TodosActions = ({ resetTodos, deleteCompletedTodos, completedTodosExis }) => {
     return (
-        <>
+        <div className={styles.todosActionsContainer}>
             <Button title="Reset Todos" onClick={resetTodos}><RiRefreshLine /></Button>
             <Button title="Clear Completed Todos" onClick={deleteCompletedTodos} disabled={!completedTodosExis}><RiDeleteBin2Line /></Button>
-        </>
+        </div>
     )
 }
 
